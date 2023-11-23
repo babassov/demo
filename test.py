5a90542804831529f77c9d4006cc912ad9c9ca48
Vincent Boyer <boyer.vincent@gmail.com>
2023-11-23 11:59:33 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,3 +3,18 @@ toto
  database = aws_lib.connect("AKIAF9BAFJKR45SBWZZ0", "hjshnk5ex5u34065AWS654/JKGjhz595d89sjkje")
 tata
  database = aws_lib.connect("AKIAF9BAFJKR45SBWZZ0", "hjshnk5ex5u34065AWS654/JKGjhz595d89sjkje")
+
+sdfs
+
+sdf
+sdf
+sdf
+sdf
+sdfs
+df
+d
+dfg
+sdf
+sdf
+ database = aws_lib.connect("AKIAF9BAFJKR45SBWZZ0", "hjshnk5ex5u34065AWS654/JKGjhz595d89sjkje")
+
