eb46247ba2d91a8959b60c256262684ce6c1934a
Vincent Boyer <boyer.vincent@gmail.com>
2023-11-23 11:51:29 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 gg-vboy-keyword001
 toto
+ database = aws_lib.connect("AKIAF9BAFJKR45SBWZZ0", "hjshnk5ex5u34065AWS654/JKGjhz595d89sjkje")
 tata
  database = aws_lib.connect("AKIAF9BAFJKR45SBWZZ0", "hjshnk5ex5u34065AWS654/JKGjhz595d89sjkje")
